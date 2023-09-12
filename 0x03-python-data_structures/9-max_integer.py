60554981352d4026c6f51efa4e36b21c3c94bdd5
RefilweM1 <RefilweMokwene908@gmail.com>
2023-09-12 14:25:14 -0700

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,13 +2,10 @@
 # 9-max_integer.py
 
 def max_integer(my_list=[]):
-    """Find the beggest integer of the list."""
     if len(my_list) == 0:
-        return (None)
-
-    big = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > big:
-            big = my_list[i]
-
-            return (big)
+        return ("None")
+    x = my_list[0]
+    for i in my_list:
+        if i > x:
+            x = i
+            return (x)
