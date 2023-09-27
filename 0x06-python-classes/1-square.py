d4e55d65441460c60b786c47388c08bbc6dc18ed
RefilweM1 <RefilweMokwene908@gmail.com>
2023-09-27 13:50:33 -0700

    2-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+def __init__(self, size):
+    """Initialize a new Square.
+
+    Args:
+    size (int):size of the new square.
+    """
+    self.__size = size
