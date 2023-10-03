8a6efb689e0da61bf183d29e3f90fd02f6bb8aca
RefilweM1 <RefilweMokwene908@gmail.com>
2023-10-03 12:48:06 -0700

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def say_my_name(first name, last name=""):
-    if not isinstance(first name, str):
-        raise TypeError("first name must be a string")
-    if not isinstance(last name, str):
-        raise TypeError("last name must be a string")
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print("My name is {:s} {:s}".format(first_name, last_name))
