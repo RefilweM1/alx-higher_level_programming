84401a4ae98604dfe37cf052a935d04c53a57b23
RefilweM1 <RefilweMokwene908@gmail.com>
2023-10-10 03:20:55 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module base_geometry
+creates a class"""
+
+class BaseGeometry:
+    """class with public instances method"""
+
+    def area(self):
+        """Raise an Exception with the message
+        'area() ia not implemented' 
+        """
+        raise Exception('area() is not implemented')
